56a3e0961f585f35b264303ffcf2bbb6554ebd9a
ERIC BESSON <e-besson@laposte.net>
2024-01-08 17:12:26 +0100

    bubble random

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const bubble = document.createElement("span");
-document.body.appendChild(bubble);
-bubble.classList.add("bubble");
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  document.body.appendChild(bubble);
+  bubble.classList.add("bubble");
 
-const size = Math.random() * 300 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+
+  bubble.style.setProperty = ("--left", Math.random() * 100 * plusMinus + "%");
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 3000);
